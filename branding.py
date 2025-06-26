fde544be98b4162efce49d880c31549a79c0884c
Bernard Wiafe Akenteng <wiafe1713@gmail.com>
2025-06-26 08:59:02 -0700

    Update branding.py

--- branding.py
+++ branding.py
@@ -1,4 +1,4 @@
-CLIENT_NAME = "Automation_hub"
+CLIENT_NAME = "Zhongmei Engineering Group"
 APP_TITLE = "Enhanced ACI 211.1 Concrete Mix Designer"
 PRIMARY_COLOR = "#0052cc"
 LOGO_PATH = "assets/client_logo.png"  # Leave blank ("") if not using a logo

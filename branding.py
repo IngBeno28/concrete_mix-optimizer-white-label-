aca62bfffea977ca16004d704ae8093ddfab8336
Bernard Wiafe Akenteng <wiafe1713@gmail.com>
2025-06-26 06:45:11 -0700

    Create branding.py

---
+++ branding.py
@@ -0,0 +1,5 @@
+CLIENT_NAME = "Automation_hub"
+APP_TITLE = "Enhanced ACI 211.1 Concrete Mix Designer"
+PRIMARY_COLOR = "#0052cc"
+LOGO_PATH = "assets/client_logo.png"  # Leave blank ("") if not using a logo
+FOOTER_NOTE = "© 2025 ACI Mix Designer | Built for engineering precision"
